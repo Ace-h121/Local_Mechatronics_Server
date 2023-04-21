269efc1adb144f5929c8265143efd26a1a7085e8
Ace Hathaway <116515120+Ace-h121@users.noreply.github.com>
2023-04-20 22:06:08 -0400

    Inital Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+const  fs  = require('fs')
+const http = require('http')
+const serverPort = 3000
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('error: file not found')
+        } else {
+            res.write(data)
+        }
+    })
+  }).listen(8080);
+
+
+const port = new SerialPort({ path: 'COM8', baudRate: 9800 })
+const parser = new ReadlineParser()
+port.pipe(parser)
+
+parser.on('data', console.log);
+
+port.write("Hello");
+
