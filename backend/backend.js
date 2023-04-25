c3333ca844472369f86525fe566906b6e6efa75e
Ace Hathaway <116515120+Ace-h121@users.noreply.github.com>
2023-04-24 22:19:39 -0400

    functioning webpage!!!

--- backend/backend.js
+++ backend/backend.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 
-const  app=express();
+const app = express();
 app.use(cors());
 
 //tells the server what to connect to, also troubleshooting to see if its connected
-app.listen(5000, ()=>console.log("Backend is connected"))
+app.listen(5000, () => console.log("Backend is connected"))
 
-app.get("/api",(req,res)=>{
-    res.json({"users":["its", "working", "omg"]})
+app.get("/api", (req, res) => {
+    res.json({ "users": ["its", "working", "omg"] })
 })
 
 // connecting to the arduino serialport
@@ -18,26 +18,26 @@ import { ReadlineParser } from '@serialport/parser-readline';
 const arduinoConnectDelay = 2000;
 
 
-  const port = new SerialPort({ path: 'COM8', baudRate: 9600,})
+const port = new SerialPort({ path: 'COM8', baudRate: 9600, })
 
-  //used so it can read incoming data from arduino
-  const parser = new ReadlineParser()
-  
-  //the arduino connect delay is used to compensate for the time it takes the arduino to restart when it is being connected to the new server
-  port.on('open', () => {
-      setTimeout(function () {
-            port.write("3");
-      }, arduinoConnectDelay)
-  })
-  
-  port.pipe(parser)
-  
-  
-  parser.on('data', function(data){
-      console.log(data);
-  
-  });
-  
-  
+//used so it can read incoming data from arduino
+const parser = new ReadlineParser()
+
+//the arduino connect delay is used to compensate for the time it takes the arduino to restart when it is being connected to the new server
+port.on('open', () => {
+    setTimeout(function () {
+        port.write("3");
+    }, arduinoConnectDelay)
+})
+
+port.pipe(parser)
+
+
+parser.on('data', function (data) {
+    console.log(data);
+
+});
+
+
 
 
--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,28 +1,45 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}]);
 
-  useEffect(()=>{
-    fetch("http://localhost:5000/api").then(
-      response => response.json()
-    ).then(
-      data => {
+  useEffect(() => {
+    fetch("http://localhost:5000/api")
+      .then((response) => response.json())
+      .then((data) => {
         setData(data);
-      }
-    )
+      });
+  }, []);
+
+  if (true){
+    return (
+      <>
+      <body>
+        <h1>
+          CONTROL THE OWLBOT!
+        </h1>
+        <input/>
+        <br/>
+        <button onClick={()=>console.log("itworked")}>
+          SUBMIT
+        </button>
+      </body>
   
-    },[])
-  return(
-    <div>
-    {(typeof data.users === 'undefined') ?(
-      <p>loading...</p>
-    ): (
-      data.users.map((user, i) => (
-        <p key={i}>{user}</p>
-      ))
-    )}
-    </div>
-  )
+      </>
+    )
+  }
+
+  const handleClick = () => console.log("it worked");
+  return (
+    <>
+      {typeof data.users === "undefined" ? (
+        <p>loading...</p>
+      ) : (
+        data.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
+        <button onClick={handleClick}> </button>
+    </>
+  );
 }
-export default App
+export default App;
